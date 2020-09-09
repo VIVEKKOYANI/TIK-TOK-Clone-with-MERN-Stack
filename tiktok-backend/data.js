14ce71c7774f5b9834bafa6348af8c18aae34f68
vivek <vivek.koyani@gmail.com>
2020-09-09 12:28:04 +0530

    add data file and mongodb connection

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: 'https://youtu.be/QUk3MNo1Exs',
+        channel:"vivek",
+        description:"wow Epic MERN stack TIK TOK",
+        song:"99 problems but React aint one",
+        likes:"111",
+        shares:"222",
+        messages:"333"
+    },
+    {
+        url: 'https://youtu.be/j38uQX-s0o8://youtu.be/QUk3MNo1Exs',
+        channel:"vivek",
+        description:"wow Epic MERN stack TIK TOK",
+        song:"99 problems but React aint one",
+        likes:"111",
+        shares:"222",
+        messages:"333"
+    }
+]
+
+export default data;
--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,5 +1,7 @@
 import exprss from "express";
 import mongoose from "mongoose";
+import Data from './data.js';
+
 
 // app config
 const app = exprss();
@@ -9,10 +11,17 @@ const port = 9000;
 
 
 //DB config
+const connection__url = 'mongodb+srv://admin:Nishit@2020@cluster0.uzjoa.mongodb.net/tiktok?retryWrites=true&w=majority';
 
+mongoose.connect(connection__url, {
+    useNewUrlParser:true,
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+})
 
 //api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
 
 // listen
 app.listen(port, () => console.log(`listning on localhost:${port}`))
