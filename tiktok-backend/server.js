6c085ec9f272f84c3dfd147c9708a7da8f909d72
vivek <vivek.koyani@gmail.com>
2020-09-09 11:52:39 +0530

    add comment on server.js

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,9 +1,18 @@
 import exprss from "express";
 import mongoose from "mongoose";
 
+// app config
 const app = exprss();
 const port = 9000;
 
+//middlware
+
+
+//DB config
+
+
+//api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World"));
 
+// listen
 app.listen(port, () => console.log(`listning on localhost:${port}`))
